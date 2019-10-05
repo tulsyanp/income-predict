9d19335353597beab987807c6751627322b58611
Prateek Tulsyan <prateek.tulsyan@decurtis.com>
2019-10-05 23:53:24 +0100

    remove unused package

--- lr_predict.py
+++ lr_predict.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
