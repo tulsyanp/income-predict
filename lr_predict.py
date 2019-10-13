75ea84aaede6178e83271af60c0419612ce3d79f
Prateek Tulsyan <tulsyanp@tcd.ie>
2019-10-13 23:44:16 +0100

    applied random forest model and removed a few outliers

--- lr_predict.py
+++ lr_predict.py
@@ -1,9 +1,5 @@
 import pandas as pd
-from sklearn.metrics import r2_score, mean_squared_error
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from main import data_pre_processing, remove_outliers, split_dataset, generate_csv
-import math
+from main import data_pre_processing, remove_outliers, split_dataset, model_linear_regression_predict, model_random_forest_predict
 
 
 # read training dataset
@@ -19,22 +15,16 @@ preProcessedDataFrame = data_pre_processing(trainData, predictionData)
 # split dataset
 trainDataFrame, predictionDataFrame = split_dataset(preProcessedDataFrame)
 
+# prediction dataset processing
+predictionDataFrame['Country'] = predictionDataFrame['Country'].fillna(round(predictionDataFrame['Country'].mean()))
+predictionDataFrame['Profession'] = predictionDataFrame['Profession'].fillna(round(predictionDataFrame['Profession'].mean()))
+
 # model training by dividing target and attributes
 X = trainDataFrame.loc[:, trainDataFrame.columns != 'Income']
 y = trainDataFrame['Income']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+# Uncomment to predict using Linear Regression Model
+# model_linear_regression_predict(X, y, predictionDataFrame)
 
-linear_regression = LinearRegression()
-linear_regression.fit(X_train, y_train)
-
-y_pred = linear_regression.predict(X_test)
-print('Variance score: %.2f' % r2_score(y_test, y_pred))
-print("Root Mean squared error: %.2f" % math.sqrt(mean_squared_error(y_test, y_pred)))
-
-
-# predicting income on prediction dataset based on trained model
-P_test = predictionDataFrame.loc[:, predictionDataFrame.columns != 'Income']
-P_pred = linear_regression.predict(P_test)
-pred_to_csv = generate_csv(predictionDataFrame, P_pred)
-print('CSV generated')
+# Uncomment to predict using Random Forest Model
+model_random_forest_predict(X, y, predictionDataFrame)
--- main.py
+++ main.py
@@ -1,47 +1,56 @@
 import pandas as pd
+import math
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.model_selection import train_test_split
 
 
 def data_pre_processing(train, predict):
-
     data = pd.concat([train, predict])
 
-    dataFrame = data.drop(columns=['Wears Glasses', 'Hair Color', 'Body Height [cm]', 'Size of City'])
+    dataFrame = data.drop(columns=['Hair Color'])
 
-    # TODO Column Year of Record (Mode/Mean)
-    dataFrame['Year of Record'].fillna(dataFrame['Year of Record'].mode()[0], inplace=True)
+    dataFrame['Year of Record'].fillna(round(dataFrame['Year of Record'].median()), inplace=True)
 
     # Column Gender Manipulations
     dataFrame['Gender'].fillna('unknown', inplace=True)
     dataFrame['Gender'].replace(['0'], ['male'], inplace=True)
     dataFrame['Gender'] = dataFrame['Gender'].str.lower()
+    dataFrame['Gender'] = dataFrame['Gender'].str.strip()
 
     # Column Age Manipulations
-    dataFrame['Age'].fillna(round(dataFrame['Age'].mean()), inplace=True)
+    dataFrame['Age'].fillna(round(dataFrame['Age'].median()), inplace=True)
 
     # Column Country Manipulation
     dataFrame['Country'] = dataFrame['Country'].str.lower()
+    dataFrame['Country'] = dataFrame['Country'].str.strip()
 
     # Column Profession Manipulation
     dataFrame['Profession'].fillna('none', inplace=True)
     dataFrame['Profession'] = dataFrame['Profession'].str.lower()
+    dataFrame['Profession'] = dataFrame['Profession'].str.strip()
+    dataFrame['Profession'] = dataFrame['Profession'].str.slice(start=0, stop=3)
+    dataFrame['Profession'] = dataFrame['Profession'].str.strip()
 
     # Column University Degree
     dataFrame['University Degree'].fillna('unknown', inplace=True)
     dataFrame['University Degree'].replace(['0'], ['No'], inplace=True)
     dataFrame['University Degree'] = dataFrame['University Degree'].str.lower()
+    dataFrame['University Degree'] = dataFrame['University Degree'].str.strip()
 
-    # encoding
-    dataFrame = pd.get_dummies(
-        dataFrame,
-        columns=["Gender", "Country", "Profession", "University Degree"],
-        prefix=["Gender", "Country", "Profession", "Degree"]
-    )
+    # cat codes/target mean encoding
+    dataFrame['Gender'] = dataFrame['Gender'].astype('category').cat.codes
+    # dataFrame['Wears Glasses'] = dataFrame['Wears Glasses'].astype('category').cat.codes
+    dataFrame['Country'] = dataFrame['Country'].map(dataFrame.groupby('Country')['Income'].mean())
+    dataFrame['Profession'] = dataFrame['Profession'].map(dataFrame.groupby('Profession')['Income'].mean())
+    dataFrame['University Degree'] = dataFrame['University Degree'].map(dataFrame.groupby('University Degree')['Income'].mean())
 
     return dataFrame
 
 
 def remove_outliers(data):
-
     data.rename(columns={'Income in EUR': 'Income'}, inplace=True)
     data.drop(data[data['Income'] < 0].index, inplace=True)
     data.drop(data['Income'].idxmax(), inplace=True)
@@ -64,3 +73,42 @@ def generate_csv(dataset, prediction):
     datasetToCSV.to_csv('submission.csv', index=False)
 
     return datasetToCSV
+
+
+def model_linear_regression_predict(X, y, predictionDataFrame):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=0)
+
+    linear_regression = LinearRegression()
+    linear_regression.fit(X_train, y_train)
+
+    y_pred = linear_regression.predict(X_test)
+    print('Internal Test: Variance score: %.2f' % r2_score(y_test, y_pred))
+    print("Internal Test: Root Mean squared error: %.2f" % math.sqrt(mean_squared_error(y_test, y_pred)))
+
+    # predicting income on prediction dataset based on trained model
+    predict(linear_regression, predictionDataFrame)
+
+
+def model_random_forest_predict(X, y, predictionDataFrame):
+    train_features, test_features, train_labels, test_labels = train_test_split(X, y, test_size=0.1, random_state=42)
+
+    random_forest = RandomForestRegressor(n_estimators=200, random_state=42)
+    random_forest.fit(train_features, train_labels)
+
+    predictions = random_forest.predict(test_features)
+    errors = abs(predictions - test_labels)
+    print('Internal Test: Mean Absolute Error:', round(np.mean(errors), 2))
+    mape = 100 * (errors / test_labels)
+    accuracy = 100 - np.mean(mape)
+    print('Internal Test: Accuracy:', round(accuracy, 2), '%.')
+    print("Internal Test: Root Mean squared error: %.2f" % math.sqrt(mean_squared_error(test_labels, predictions)))
+
+    # predicting income on prediction dataset based on trained model
+    predict(random_forest, predictionDataFrame)
+
+
+def predict(model, predictionDataFrame):
+    P_test = predictionDataFrame.loc[:, predictionDataFrame.columns != 'Income']
+    P_pred = model.predict(P_test)
+    generate_csv(predictionDataFrame, P_pred)
+    print('Predicted CSV generated')
